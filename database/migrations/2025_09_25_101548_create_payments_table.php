fe279f4c191e13df6668b90b6ac395b494e448e6
Jeffry <jeffryharfouche@hotmail.com>
2025-10-04 13:33:31 +0300

    Add payments entity

--- database/migrations/2025_09_25_101548_create_payments_table.php
+++ database/migrations/2025_09_25_101548_create_payments_table.php
@@ -14,7 +14,11 @@ return new class extends Migration
         Schema::create('payments', function (Blueprint $table) {
             $table->id();
             $table->foreignId('booking_id')->constrained()->onDelete('cascade'); // link to bookings table
+            $table->foreignId('user_id')->constrained()->onDelete('cascade'); // link to users table
+            $table->foreignId('property_id')->constrained()->onDelete('cascade'); // link to properties table
+            $table->decimal('amount', 10, 2); // e.g., 99999999.99
             $table->string('payment_method'); // e.g., credit card, PayPal
+            $table->string('transaction_id')->unique(); // unique transaction identifier
             $table->string('status')->default('pending'); // e.g., pending, completed, failed
             $table->timestamps();
         });
